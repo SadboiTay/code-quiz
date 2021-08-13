b871efe863b6d253b95dd492fbfd89fb98688d33
SadboiTay <tg.hakes@gmail.com>
2021-08-13 16:11:29 -0600

    start getQuestions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,98 @@
-// variables
+// variables for DOM elements
 var startBtnEl = document.querySelector('#start-btn')
 var startPageEl = document.querySelector('#start-page');
 var questionsPageEl = document.querySelector('#questions');
 var timeEl = document.querySelector('#time');
+var questionEl = document.querySelector('#Q');
+var choicesEl = document.querySelector('#choices');
 
+// variable for timer and questions array
 var time = 60;
+var currentQuestionIndex = 0;
 
-// create questions array
+// create questions array with choices and answers
 var questionsArr = [
     {
-        q: "Who?"
+        q: "Who?",
+        choices: ["yes", "no", "maybe", "for sure"],
+        answer: "for sure"
     },
     {
-        q: "What?"
+        q: "What?",
+        choices: ["yes", "no", "maybe", "for sure"],
+        answer: "for sure"
     },
     {
-        q: "When?"
+        q: "When?",
+        choices: ["yes", "no", "maybe", "for sure"],
+        answer: "for sure"
     },
     {
-        q: "Where?"
+        q: "Where?",
+        choices: ["yes", "no", "maybe", "for sure"],
+        answer: "for sure"
     },
     {
-        q: "Why?"
+        q: "Why?",
+        choices: ["yes", "no", "maybe", "for sure"],
+        answer: "for sure"
     }
 ]
 
 // functions
-    // show hidden questions div and start timer
-    var startQuiz = function() {
-        // hide startPage div
-        startPageEl.className = "hide";
+var startQuiz = function() {
+    // hide startPage div
+    startPageEl.style.display = "none";
 
-        // show questions div
-        questionsPageEl.style.display = "block";
+    // show questions div
+    questionsPageEl.style.display = "block";
 
-        // start timer with setInterval
-        var timeCountdown = setInterval(function() {
-            // show timeEl
-            timeEl.textContent = time;
-            if (time > 0) {
-                time--;
-            } 
+    // start timer with setInterval
+    var timerInterval = setInterval(timeCountdown, 1000);
 
-        }, 1000);
+    // get questions
+    getQuestion();
+}
 
-        console.log(questionsArr[1]);
+var getQuestion = function() {
+    // loop through questions array
+    for (var i = 0; i < questionsArr.length; i++) {
+        // set question to questionEl
+        questionEl.textContent = questionsArr[i].q
+
+        // loop over choices
+        questionsArr[i].choices.forEach(function(choice, i)
+        {
+            // create new button for each choice
+            var choiceButton = document.createElement("button");
+            choiceButton.setAttribute("class", "btn");
+            choiceButton.setAttribute("value", choice);
+
+            choiceButton.textContent = i + "hello there";
+
+            // display choice on page 
+            choicesEl.appendChild(choiceButton);
+        });
     }
-// end functions
+    
+}
+
+var timeCountdown = function() {
+    // show timeEl
+    timeEl.textContent = time;
+
+    // tick down
+    if (time > 0) {
+        time--;
+    } else {
+        endQuiz();
+    }
+}
 
 // on click 'start quiz', show hidden questions div and start timer
 startBtnEl.addEventListener("click", startQuiz);
 
 
-// loop through questions array to pull one
 
 // right answer give 'correct' response - go to next question
 
